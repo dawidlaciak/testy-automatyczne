61d78c5b9a4dc23c6e7a336f4a37be044f73bf60
Dawid Łaciak <dawidlaciak02@gmail.com>
2024-12-26 20:36:16 +0100

    checkbox.cy.js changes

--- cypress/e2e/checkbox.cy.js
+++ cypress/e2e/checkbox.cy.js
@@ -5,7 +5,7 @@ import Women from '../pages/Women';
 describe('Test związany z zaznaczaniem checkboxa', () => {
 	it('Kliknięcie w zakładkę Woman', () => {
 		cy.visit('/');
-		cy.get('a[title="Womann"]').click();
+		cy.get('a[title="Women"]').click();
 	});
 
 	it('Zaznaczenie checkboxa w zakładce Woman', () => {
